79a1c7879f6254c7454721190caebe4c006e8dba
Eryk Fryderyk <grzeniu098@gmail.com>
2020-06-17 22:42:08 +0200

    change project section and add function for skill section

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,8 @@ const prepareDOMElements = () => {
     bodyOverly = document.querySelector('.mobile-body-overly');
     body = document.getElementById('home');
     header = document.querySelector('.header-section');
+    boxLanguage = document.querySelector('.language-box');
+    contentElements = document.querySelectorAll('.text-skill');
     appearElements = document.querySelectorAll('.appear-animation');
     opacityElements = document.querySelectorAll('.opacity-animation');
     clickMobileLink = document.querySelectorAll('.mobile-nav-menu__link');
@@ -18,11 +20,13 @@ const prepareDOMElements = () => {
 
 
 const prepareDOMEvents = () => {
-    hamburger.addEventListener('click', mobileMenu);
-    bodyOverly.addEventListener('click', closeMenu);
     window.addEventListener('scroll', headerDarken);
     window.addEventListener('scroll', scrollAppear);
     window.addEventListener('scroll', scrollOpacity);
+
+    hamburger.addEventListener('click', mobileMenu);
+    bodyOverly.addEventListener('click', closeMenu);
+    boxLanguage.addEventListener('click', e => contentLanguageShow(e));
 }
 
 
@@ -82,6 +86,51 @@ const scrollOpacity = () => {
     })
 
 }
+
+
+// test function
+const contentLanguageShow = (e) => {
+    const attribute = e.target.getAttribute('href');
+
+    if(attribute === "#html"){
+        contentElements.forEach(element => {
+            element.classList.add('content--hidden');
+        });
+        contentElements[0].classList.remove('content--hidden');
+
+    } else if(attribute === "#css"){
+        contentElements.forEach(element => {
+            element.classList.add('content--hidden');
+        });
+        contentElements[1].classList.remove('content--hidden');
+
+    } else if (attribute === "#javascript") {
+        contentElements.forEach(element => {
+            element.classList.add('content--hidden');
+        });
+        contentElements[2].classList.remove('content--hidden');
+
+    } else if (attribute === "#php") {
+        contentElements.forEach(element => {
+            element.classList.add('content--hidden');
+        });
+        contentElements[3].classList.remove('content--hidden');
+
+    } else if (attribute === "#mysql") {
+        contentElements.forEach(element => {
+            element.classList.add('content--hidden');
+        });
+        contentElements[4].classList.remove('content--hidden');
+
+    } else if (attribute === "#jquery") {
+        contentElements.forEach(element => {
+            element.classList.add('content--hidden');
+        });
+        contentElements[5].classList.remove('content--hidden');
+        
+    }
+}
+
 
 
 window.addEventListener('DOMContentLoaded', main);
